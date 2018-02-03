3e188f6a75c3e9e38cbcfce434ac1be605984cb6
Wei Wang <wvw@google.com>
2018-02-02 21:32:05 -0800

    zenfone7: Remove QTI perfd
    Bug: 69270928
    Test: Build and boot device and test camera
    Change-Id: I87b283206f462fb5c8ec9cdd303ae2934fe9bfc4
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- device.mk
+++ device.mk
@@ -314,10 +314,6 @@ PRODUCT_PACKAGES += \
     SecureElement \
     com.android.nfc_extras
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.3
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr
--- extract-files.py
+++ extract-files.py
@@ -56,8 +56,6 @@ blob_fixups: blob_fixups_user_type = {
     ('system/lib64/libxditk_ISP.so',
      'system/lib64/libxditk_ditArchLIB.so'): blob_fixup()
         .replace_needed('libOpenCL.so', 'libOpenCL_system.so'),
-     'system_ext/lib64/libqti-iopd-client_system.so': blob_fixup()
-        .replace_needed('vendor.qti.hardware.iop@2.0.so', 'vendor.qti.hardware.iop@2.0_system.so'),
     'vendor/etc/msm_irqbalance.conf': blob_fixup()
         .regex_replace('IGNORED_IRQ=27,23,38$', 'IGNORED_IRQ=27,23,38,115,332'),
     'vendor/etc/seccomp_policy/qspm.policy': blob_fixup()
@@ -67,9 +65,6 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib/libreference-ril.so',
      'vendor/lib64/libreference-ril.so'): blob_fixup()
         .add_needed('libbase_shim.so'),
-    ('vendor/lib/libqti-perfd.so',
-     'vendor/lib64/libqti-perfd.so'): blob_fixup()
-        .binary_regex_replace(b'sys.asus.dongletype', b'vendor.sys.asus.dongletype'),
     'vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so': blob_fixup()
         .replace_needed('libhidltransport.so', 'libhidlbase_shim.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
