c6c7d5f7760418db440f9c2e16bfdeef0fb1bd92
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2021-03-22 15:58:08 +0100

    zenfone7: Import powerhint.json from android-11.0.0_r32
    Change-Id: I552870c130ceaac4d20322acf6d1da22ad5017ec
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- device.mk
+++ device.mk
@@ -261,6 +261,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Prebuilt
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,device/asus/zenfone7/prebuilt/product,product) \
