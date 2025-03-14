422424c08d5c486c1f2c690cf8dbea7ded9e7f96
BrightTwikling <DF.SJbbba5@gmail.com>
2025-03-15 06:35:45 +0900

    Drop android.hardware.boot@1.0
    Signed-off-by: BrightTwikling <DF.SJbbba5@gmail.com>

--- device.mk
+++ device.mk
@@ -81,10 +81,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration_ZS670KS.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes_ZS670KS.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes_ZS670KS.xml
 
-# Boot control
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-service
-
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
