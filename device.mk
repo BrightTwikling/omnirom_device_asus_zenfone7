8674592dafe3a5c9fe6527da55851c053176d1be
BrightTwikling <DFSJbbba5@gmail.com>
2025-04-29 22:25:23 +0900

    Switch to QTI bootctrl HAL
    Signed-off-by: BrightTwikling <DFSJbbba5@gmail.com>

--- device.mk
+++ device.mk
@@ -118,9 +118,9 @@ PRODUCT_COPY_FILES +=\
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl.recovery \
-    android.hardware.boot@1.1-service \
-    bootctrl.kona.recovery
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
