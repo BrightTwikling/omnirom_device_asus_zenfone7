7206a665d133fb907551f542a25cdeec0cf26499
BrightTwikling <DFSJbbba5@gmail.com>
2025-05-01 20:42:55 +0900

    Switch to QTI health implementation
    Inspired by following commit
    
    https://github.com/AlphaDroid-devices/device_xiaomi_ginkgo/commit/2fa657
    
    Signed-off-by: BrightTwikling <DF.SJbbba5@gmail.com>

--- device.mk
+++ device.mk
@@ -251,7 +251,7 @@ endif
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl-qti \
     android.hardware.health@2.1-service
 
 # Health for charing control
